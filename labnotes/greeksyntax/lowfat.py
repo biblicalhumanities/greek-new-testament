52dafe16636d3bf6466b26c6307b6bb9a49750e0
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2017-12-06 15:29:05 -0500

    Now ready to go public.

---
+++ labnotes/greeksyntax/lowfat.py
@@ -0,0 +1,143 @@
+import os
+from BaseXClient import BaseXClient
+
+from IPython.display import HTML
+from pygments import highlight
+from pygments.lexers import XmlLexer
+from pygments.formatters import HtmlFormatter
+from IPython.display import HTML
+
+def wrap(query):
+	return "<results xmlns:xi='http://www.w3.org/2001/XInclude'>{" + query + "}</results>"
+
+def pretty(xml):
+	formatter = HtmlFormatter()
+	display(
+		HTML('<style type="text/css">{}</style>{}'.format (
+			formatter.get_style_defs('.highlight'),
+			highlight(xml, XmlLexer(), formatter))))
+
+def milestone(m):
+	if m.count("!") == 1:
+		return "//w[@osisId='" + m + "']"
+	elif m.count(".") == 2:
+		return "//sentence[milestone[@id='" + m + "']]"
+	else:
+		return "//sentence[milestone[starts-with(@id,'" + m + "')]]"
+
+
+def highlight_query_string(query):
+	return r"""
+		for $h in """ + query + r"""
+		let $sentence := $h/ancestor::sentence
+		let $sentencewords :=
+				for $w in $sentence/descendant-or-self::w
+				order by $w/@n
+				return $w
+		let $hitwords :=
+				for $w in $h/descendant-or-self::w
+				order by $w/@n
+				return $w
+		return
+				<p>
+					<b>
+					{
+						$sentence/milestone/@id ! string(.)
+					}
+					</b>
+					<br/>
+					{
+						for $s in $sentencewords
+						let $title := attribute title { $s ! (@class, ": ", @lemma, @number, @gender, @case, @tense, @voice, @mood)}
+						let $content := string($s)
+						return
+							if ($s/@n = $hitwords/@n)
+								then <w style="color:blue">{ $title, $content }</w>
+								else <w>{ $title, $content }</w>
+					}
+				</p>"""
+
+def morph_query_string(query):
+	return r"""
+		for $h in """ + query + r"""
+		let $words := $h/descendant-or-self::w
+		return
+			<p>
+				<b>
+				{
+					$words[@n=min($words/@n)]/@osisId ! string(.)
+				}
+				</b>
+				{" "}
+				{
+					for $w in $words
+					order by $w/@n
+					return
+						<w>
+							{ attribute title {$w ! (@class, ": ", @lemma, @number, @gender, @case, @tense, @voice, @mood)}}
+							{ $w ! string(.) }
+						</w>
+				}
+			</p>"""
+
+
+def sentence_query_string(query):
+	return r"""
+		for $h in """ + query + r"""
+		let $sentence := $h/ancestor::sentence
+		let $sentencewords :=
+				for $w in $sentence/descendant-or-self::w
+				order by $w/@n
+				return $w
+		let $hitwords :=
+				for $w in $h/descendant-or-self::w
+				order by $w/@n
+				return $w
+		return
+			<p>
+				<b>
+				{
+					$sentence/milestone/@id ! string(.)
+				}
+				</b>
+				{" "}
+				{
+					$sentencewords !
+						<w>
+							{ attribute title {@class, ": ", @lemma, @number, @gender, @case, @tense, @voice, @mood}}
+							{ string(.) }
+						</w>
+				}
+				<br/>
+				{ "➡️ " }
+				<b>{ $hitwords[1]/@osisId ! string(.) }</b>
+				{ " " }
+				{
+					$hitwords ! string(.)
+				}
+			</p>"""
+
+class lowfat:
+	session = {}
+
+	def __init__(self, dbname):
+		self.session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
+		self.session.execute("open " + dbname)
+
+	def xquery(self, query):
+		return self.session.query(query).execute()
+
+	def find(self, query):
+		display(HTML(self.xquery(morph_query_string(query))))
+
+	def highlight(self, query):
+		display(HTML(self.xquery(highlight_query_string(query))))
+
+	def raw(self, m):
+		print(self.xquery(
+			"<results xmlns:xi='http://www.w3.org/2001/XInclude'>{"
+			   + milestone(m)
+			+"}</results>"))
+
+	def sentence(self, query):
+		display(HTML(self.xquery(sentence_query_string(query))))
